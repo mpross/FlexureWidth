68b7d4f830864f741a704d7a63c979cb2034f75a
M.P.Ross <mross444@gmail.com>
2017-12-19 15:48:56 -0800

    Changed zoom level.

--- FlexureMeasure.py
+++ FlexureMeasure.py
@@ -8,23 +8,26 @@ f = tkFileDialog.askopenfilename()
 root.withdraw()
 data=np.genfromtxt(f)
 x=[]
+zoom=150
 maxY=max(data[:,1])
 minY=min(data[:,1])
 maxX=max(data[:,0])
 minX=min(data[:,0])
 
-data = np.array([data[k] for k in xrange(len(data)) 
-                       if data[k,0]<maxX/100 and data[k,0]>minX/100
-                       and data[k,1]<maxY/100 and data[k,1]>minY/100])
 
-left= np.array([data[k] for k in xrange(len(data)) 
-                       if data[k,0]<0])
-right= np.array([data[k] for k in xrange(len(data)) 
-                       if data[k,0]>0])
+cut = np.array([data[k] for k in xrange(len(data)) 
+                       if data[k,0]<maxX/zoom and data[k,0]>minX/zoom
+                       and data[k,1]<maxY/zoom and data[k,1]>minY/zoom])
+
+left= np.array([cut[k] for k in xrange(len(cut)) 
+                       if cut[k,0]<0])
+right= np.array([cut[k] for k in xrange(len(cut)) 
+                       if cut[k,0]>0])
 
 print(np.mean(right[:,0])-np.mean(left[:,0]))
 
-plt.plot(data[:,0],data[:,1],'ro')
+plt.plot(cut[:,0],cut[:,1],'ro')
+plt.axis([-0.1,0.1,-0.1,0.1])
 plt.show()
 
 
